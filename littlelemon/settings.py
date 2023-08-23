8c60205afd941f508d91f4629fd85cf79a8c9f0f
lyle <lylekaplandeveloper@gmail.com>
2023-08-23 19:51:38 -0400

    added models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,20 +74,26 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'my_database',  
-        'USER': 'root',  
-        'PASSWORD': 'your_password',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
-    }  
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'LittleLemon',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#             }
+#         }
+#     }
 
 
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField()  # Removed max_length
+    booking_date = models.DateTimeField()
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
