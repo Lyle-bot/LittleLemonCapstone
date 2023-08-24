ba0602d95400d6b63e7aeabd56bbbe8f111d1fea
lyle <lylekaplandeveloper@gmail.com>
2023-08-24 17:51:27 -0400

    added booking serializer and router with view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,24 @@
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
+from rest_framework import viewsets
+from .models import Booking  # Assuming Booking model is in the same app's models.py
+from .serializers import BookingSerializer 
 
-from django.shortcuts import render
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated] 
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated] 
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated] 
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
